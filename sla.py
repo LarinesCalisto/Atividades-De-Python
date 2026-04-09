3e6c17fd79034bb26398b9b252a735b85321fb6f
Larissa Inês Calisto <larissa.calisto.oliveira@gmail.com>
2026-04-09 12:50:36 -0300

    Add files via upload

---
+++ sla.py
@@ -0,0 +1,15 @@
+#Program pra perguntar a idade e falar se pode tirar CNH
+idade = int(input("Digite a sua idade: "))
+sexo= input("Digite seu gênero(masculino ou feminino):")
+if idade >= 18:
+    print("Você pode ter CNH")
+else: 
+    print("Você ainda não pode ter CNH")
+    
+#Identificar se é homem e se deve ou não se alistar o exército
+if sexo == "masculino" and idade >=18:
+     print("Se aliste no exército")
+else:
+    print("Por enquanto vc esta liberado!")
+    
+print("Fim do programa")
